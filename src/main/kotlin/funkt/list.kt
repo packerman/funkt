4d4a8585f424fb8a38ff71e09b9144709cd6356f
Piotr Ackermann <pioacker@gmail.com>
2019-12-10 21:30:36 +0100

    Basic functions on lists and options

---
+++ src/main/kotlin/funkt/list.kt
@@ -0,0 +1,37 @@
+package funkt
+
+sealed class List<out A> {
+
+    abstract fun isEmpty(): Boolean
+
+    fun find(p: (A) -> Boolean): Option<A> = when (this) {
+        is Nil -> Option()
+        is Cons -> if (p(head)) Option(head) else tail.find(p)
+    }
+
+    fun cons(a: @UnsafeVariance A): List<A> = Cons(a, this)
+
+    internal object Nil : List<Nothing>() {
+
+        override fun isEmpty(): Boolean = true
+    }
+
+    internal class Cons<A>(val head: A, val tail: List<A>) : List<A>() {
+
+        override fun isEmpty(): Boolean = false
+    }
+
+    companion object {
+
+        operator fun <A> invoke(vararg elements: A): List<A> =
+            elements.foldRight(Nil as List<A>) { e, l -> Cons(e, l) }
+    }
+}
+
+typealias Assoc<A, B> = List<Pair<A, B>>
+
+fun <A, B> Assoc<A, B>.assoc(a: A, b: B): Assoc<A, B> = cons(a to b)
+
+fun <A, B> Assoc<A, B>.lookup(a: A): Option<B> = find { it.first == a }.map { it.second }
+
+fun <A, B> emptyAssoc(): Assoc<A, B> = List()
